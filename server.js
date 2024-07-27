ede46188d2319cb0afea35c91ec1ce622dd2269d
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2024-07-27 08:41:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Store conversation history for each session
 const conversationHistory = {};
